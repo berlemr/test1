2763cb82ef5e9eda273822a9aeaf6fc2af3a50a3
rendog <rene.berlemont@gmail.com>
2020-06-24 20:38:23 +0100

    ps1 param test

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,15 @@
 param(
-	[string]$env = "dev"
+	[string]$env
 )
 
 
 if(!(test-path .\venv)){
 	& c:\users\reneb\mycode\tcity_testrunner\venv\scripts\python.exe -m venv venv
 }
+
+$env > environment.txt
+write-host $env
+
 venv\scripts\activate
 python -m pip install --upgrade pip
 pip install pytest
